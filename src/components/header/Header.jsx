896dc82a60e4e84abf372f48fa04aa74c2d3f35c
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2022-12-21 15:55:13 -0300

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="top">
-            <img className="bubble" src="./images/bubble.gif" alt=""  />
-            <img className="meteoro" src="./images/meteoro.webp" alt="" width={100} />
+            <img className="bubble" src="./images/bubble.gif" alt="" loading="lazy" />
+            <img className="meteoro" src="./images/meteoro.webp" alt="" width={100}  loading="lazy"/>
             <div className="left">
-                <img src="./images/astronauta.png" alt="" width={300} />
-                <img src="./images/planet2.webp" alt="" width={100} />
-                <img src="./images/planet3.webp" alt="" width={100} />
+                <img src="./images/astronauta.png" alt="" width={300} loading="lazy"/>
+                <img src="./images/planet2.webp" alt="" width={100}   loading="lazy"/>
+                <img src="./images/planet3.webp" alt="" width={100}   loading="lazy"/>
             </div>
             <div className="center">
                 <span>hi there!</span>
@@ -23,14 +23,14 @@ export default function Header() {
                 </div>
             </div>
             <div className="right">
-                <img src="./images/planet4.webp" alt="" width={300} />
-                <img src="./images/rocket.webp" alt="" width={50} />
+                <img src="./images/planet4.webp" alt="" width={300} loading="lazy" />
+                <img src="./images/rocket.webp" alt="" width={50}  loading="lazy"/>
             </div>
         </div>
         <div className="bottom">
-            <img src="./images/cloud.webp" alt="" width={800}/>
-            <img src="./images/planet.webp" alt="" width={700} />
-            <img src="./images/cloud.webp" alt="" width={800}/>
+            <img src="./images/cloud.webp" alt="" width={800}  loading="lazy"/>
+            <img src="./images/planet.webp" alt="" width={700} loading="lazy"/>
+            <img src="./images/cloud.webp" alt="" width={800}  loading="lazy"/>
         </div>
     </div>
   )
