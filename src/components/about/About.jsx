1ef0a26dcd68a27cba791f68730d0ecb2879206d
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2022-12-26 21:35:56 -0300

    scroll about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,66 @@
 import './about.scss'
 import {aboutme} from '../../data.js'
+import { useRef,useEffect,useState  } from 'react'
 
 export default function About() {
+
+  const slide = useRef(null);
+  const wrapper = useRef(null);
+  const[ball, setBall] = useState(['active', 0, 0])
+  
+  const changePosition = ( scrollPosition) => {
+    slide.current.scrollLeft = scrollPosition
+  }
+
+useEffect(() => {
+  if(slide.current) {
+    slide.current.addEventListener("scroll", handleScroll)
+  }
+},)
+
+
+const handleScroll = () => {
+  let scrollBall = ['active', 0, 0];
+  let scrollLeft = slide.current.scrollLeft
+  let centerScroll = wrapper.current.offsetWidth;
+  let endScroll = wrapper.current.offsetWidth * 2;
+  
+  if(scrollLeft == 0) { //start
+     scrollBall = ['active', 0, 0];
+  }
+  else if(scrollLeft >= centerScroll && scrollLeft < endScroll ){ //center
+    scrollBall = [0, 'active', 0];
+  }
+  else if(scrollLeft >= endScroll ){ //end
+    scrollBall = [0, 0, 'active'];
+  }
+
+  setBall(scrollBall)
+}
+
+
+
   return (
     <div className='about'>
         <div className="left">
-            {aboutme.map((item,key) => (
-              <div className="wrapper" key={key}>
-                  <div className="line"></div>
-                  <h1>{item.title}</h1>
-                  <span>{item.desc}</span>
-                  <div className="navigation">
-                      <div className="ball active"></div>
-                      <div className="ball"></div>
-                      <div className="ball"></div>
-                  </div>
-              </div>
-            ))}
+            <div className="content" ref={slide}>
+              {aboutme.map((item,key) => (
+                <div className="wrapper" key={key} ref={wrapper}>
+                    <div className="line"></div>
+                    <h1>{item.title}</h1>
+                    <span>{item.desc}</span>
+                </div>
+              ))}
+            </div>
+            <div className="navigation">
+                <div className={"ball " + ball[0]} onClick={() => changePosition( 0)} ></div>
+                <div className={"ball " + ball[1]} onClick={() => changePosition( wrapper.current.offsetWidth)} ></div>
+                <div className={"ball " + ball[2]} onClick={() => changePosition( wrapper.current.offsetWidth * 2)} ></div>
+            </div>
         </div>
         <div className="right">
             <img src="./images/triangle.webp" alt="" width={400} height={400} loading="lazy"/>
-            <img src="./images/astronaut-floating2.png" alt="" width={400} height={400} loading="lazy" />
+            <img src="./images/astrounaut-coffe.webp" alt="" width={300} height={300} loading="lazy" />
         </div>
     </div>
   )
