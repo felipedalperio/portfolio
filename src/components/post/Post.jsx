4053b14ef7a3157c2e121670409afe2e864982ba
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2022-12-26 21:45:12 -0300

    img width

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,7 +7,7 @@ export default function Post() {
        {projects.map((item,key) => (
            <div class="card" key={key}>
               <div class="card-info">
-                <img src={item.image} alt={item.image} loading='lazy' />
+                <img src={item.image} alt={item.image} loading='lazy' width={400} height={200} />
                 <div className="content">
                     <h1>{item.title}</h1>
                     <div className="line"></div>
